98cd68ca01c6981a5df4882c61e95c8d3caebc85
codebot-robot <codebot-robot@google.com>
2026-02-24 20:55:02 +0000

    fixup! Address review feedback: rename 'any' linter components to 'replace-empty-interface-with-any'

--- ap/pkg/config/config.go
+++ ap/pkg/config/config.go
@@ -43,10 +43,10 @@ type GovulncheckConfig struct {
 }
 
 type LintConfig struct {
-	Unused           *UnusedConfig           `json:"unused"`
-	TestContext      *TestContextConfig      `json:"testcontext"`
-	UnusedParameters *UnusedParametersConfig `json:"unusedparameters"`
-	ReplaceEmptyInterfaceWithAny *ReplaceEmptyInterfaceWithAnyConfig `json:"replace-empty-interface-with-any"`
+	Unused                       *UnusedConfig                       `json:"unused"`
+	TestContext                  *TestContextConfig                  `json:"testcontext"`
+	UnusedParameters             *UnusedParametersConfig             `json:"unusedparameters"`
+	ReplaceEmptyInterfaceWithAny *ReplaceEmptyInterfaceWithAnyConfig `json:"replaceEmptyInterfaceWithAny"`
 }
 
 type UnusedConfig struct {
@@ -144,7 +144,7 @@ func (c *Config) IsTestContextError() bool {
 	return false
 }
 
-// IsReplaceEmptyInterfaceWithAnyEnabled returns true if any detection is enabled in the config (defaulting to true).
+// IsReplaceEmptyInterfaceWithAnyEnabled returns true if the replace-empty-interface-with-any linter is enabled in the config (defaulting to true).
 func (c *Config) IsReplaceEmptyInterfaceWithAnyEnabled() bool {
 	if c.Lint != nil && c.Lint.ReplaceEmptyInterfaceWithAny != nil && c.Lint.ReplaceEmptyInterfaceWithAny.Enabled != nil {
 		return *c.Lint.ReplaceEmptyInterfaceWithAny.Enabled
